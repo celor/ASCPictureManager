fb3565823cb1a0896202cf7bc30bd88ffa8bc744
Aurélien Scelles <aurelienscelles@Aureliens-MacBook-Pro.local>
2014-03-22 11:51:13 +0100

    Fix iOS 6 pbm

--- Classes/ASCPictureManager.m
+++ Classes/ASCPictureManager.m
@@ -201,11 +201,40 @@
 
 static inline NSString *ImageSavePathFromKey(NSString *key) {
     
-#if (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000) || (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1090)
-	return [[NSString alloc] initWithFormat:@"%@/images/%@", [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject], [[[key dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:0] stringByReplacingOccurrencesOfString:@"/" withString:@"_"]];
-#else
-	return [[NSString alloc] initWithFormat:@"%@/images/%@", [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject], [[[key dataUsingEncoding:NSUTF8StringEncoding] base64Encoded] stringByReplacingOccurrencesOfString:@"/" withString:@"_"]];
-#endif
+    if ([NSData instancesRespondToSelector:@selector(base64EncodedStringWithOptions:)]) {
+        
+        return [[NSString alloc] initWithFormat:@"%@/images/%@", [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject], [[[key dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:0] stringByReplacingOccurrencesOfString:@"/" withString:@"_"]];
+    }
+    else {
+        NSData *keyData = [key dataUsingEncoding:NSUTF8StringEncoding];
+        const uint8_t* input = (const uint8_t*)[keyData bytes];
+        NSInteger length = [keyData length];
+        
+        static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+        
+        NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
+        uint8_t* output = (uint8_t*)data.mutableBytes;
+        
+        NSInteger i;
+        for (i=0; i < length; i += 3) {
+            NSInteger value = 0;
+            NSInteger j;
+            for (j = i; j < (i + 3); j++) {
+                value <<= 8;
+                
+                if (j < length) {
+                    value |= (0xFF & input[j]);
+                }
+            }
+            
+            NSInteger theIndex = (i / 3) * 4;
+            output[theIndex + 0] =                    table[(value >> 18) & 0x3F];
+            output[theIndex + 1] =                    table[(value >> 12) & 0x3F];
+            output[theIndex + 2] = (i + 1) < length ? table[(value >> 6)  & 0x3F] : '=';
+            output[theIndex + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
+        }
+        return [[NSString alloc] initWithFormat:@"%@/images/%@", [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject], [[[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding]stringByReplacingOccurrencesOfString:@"/" withString:@"_"]];
+    }
 }
 
 - (id)savedObjectForKey:(id)key {
